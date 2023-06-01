0fd37fb07e119ca51afb5d6e0b3dddc808d7fa16
nakv <ng.anhkhoacontact@gmail.com>
2023-06-01 12:50:49 +0700

    User-admin management

--- apis/user.api.ts
+++ apis/user.api.ts
@@ -14,27 +14,27 @@ export const updatePassword = async (data: {
   return httpRequest.put('/profile/password', data);
 };
 export const getUserForAdmin = async (
-  current_page: number,
-  per_page = 10,
+  page: number,
+  limit = 10,
   searchFields = [],
   value = ''
 ) => {
   return httpRequest.get(
-    `/admin/users?per_page=${per_page}&page=${current_page}${createSearchParams(
+    `/users?limit=${limit}&page=${page}${createSearchParams(
       searchFields,
       value
     )}`
   );
 };
 export const activeUser = async (data: any) => {
-  return httpRequest.put('/admin/users/active', data);
+  return httpRequest.put('/users/activate', data);
 };
 export const unactiveUser = async (data: any) => {
-  return httpRequest.put('/admin/users/unactive', data);
+  return httpRequest.put('/users/deactivate', data);
 };
 export const assignRole = async (data: any) => {
-  return httpRequest.post('/admin/users/assign-role', data);
+  return httpRequest.put('/users/assign-role', data);
 };
 export const removeRole = async (data: any) => {
-  return httpRequest.put('/admin/users/remove-role', data);
+  return httpRequest.put('/users/remove-role', data);
 };
