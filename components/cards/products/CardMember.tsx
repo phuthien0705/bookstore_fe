a7a8011dfb6ccdc53229c6a152af6b51b412bf4d
phuthien0705 <huaphuthienksst@gmail.com>
2023-06-09 00:23:33 +0700

    change content of about-us page

--- components/cards/products/CardMember.tsx
+++ components/cards/products/CardMember.tsx
@@ -1,17 +1,5 @@
-import { Card, Grid, CardMedia, Typography, CardContent } from '@mui/material';
-import Image from 'next/image';
-import LogosloganMd from '/assets/images/boxo/Logoslogan-md.png';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import Phu from '/assets/images/boxo/giaphu.jpg';
-import DKhoa from '/assets/images/boxo/dinhkhoa.jpg';
-import Khanh from '/assets/images/boxo/khanh.jpg';
-import Loc from '/assets/images/boxo/loc.jpg';
-import Thien from '/assets/images/boxo/thien.jpg';
-import AKhoa from '/assets/images/boxo/anhkhoa.jpg';
-import Long from '/assets/images/boxo/long.jpg';
+import { Card, Grid, CardMedia } from '@mui/material';
 const CardMember = () => {
-  const matches = useMediaQuery('(min-width:700px)');
-
   return (
     <>
       <Grid item xs={12} sm={6} md={4} lg={3}>
@@ -20,15 +8,15 @@ const CardMember = () => {
           variant="outlined"
           sx={{
             maxWidth: 300,
-
             borderWidth: 1,
           }}
         >
           <CardMedia
             component="img"
-            image="img/giaphu.jpg"
+            image="img/thien.jpg"
             height="300"
-            title="Phú"
+            title="Thiên"
+            sx={{ height: 300 }}
           />
         </Card>
       </Grid>
@@ -44,108 +32,55 @@ const CardMember = () => {
         >
           <CardMedia
             component="img"
-            image="img/thien.jpg"
+            image="img/giaphu.jpg"
             height="300"
-            title="Thiên"
+            title="Phú"
+            sx={{ height: 300 }}
           />
         </Card>
-      </Grid>{' '}
-      {/* <Grid item xs={12} sm={6} md={4} lg={3}>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4} lg={3}>
         <Card
+          className="shadow"
           variant="outlined"
           sx={{
             maxWidth: 300,
-            height: 500,
             borderWidth: 1,
-            borderColor: '#2196f3',
           }}
         >
           <CardMedia
             component="img"
             image="img/khanh.jpg"
             height="300"
-            title="Khánh"
+            title="Khanh"
+            sx={{ height: 300 }}
           />
-
-          <CardContent>
-            <Typography gutterBottom variant="h4">
-              Phạm Đức Khánh - 20521453
-            </Typography>
-            <br />
-            <Typography variant="h5">
-              <strong>Team: </strong>Back-end
-              <br />
-              <strong>Nhiệm vụ: </strong> Lập trình API, Controller, Model, tạo
-              Dataset.
-            </Typography>
-          </CardContent>
         </Card>
-      </Grid>{' '} */}
-      {/* <Grid item xs={12} sm={6} md={4} lg={3}>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4} lg={3}>
         <Card
+          className="shadow"
           variant="outlined"
           sx={{
             maxWidth: 300,
-            height: 500,
             borderWidth: 1,
-            borderColor: '#2196f3',
           }}
         >
           <CardMedia
             component="img"
             image="img/anhkhoa.jpg"
             height="300"
-            title="Anh Khoa"
+            title="AnhKhoa"
+            sx={{ height: 300 }}
           />
-          <CardContent>
-            <Typography gutterBottom variant="h4">
-              Nguyễn Anh Khoa - 20520584
-            </Typography>
-            <br />
-            <Typography variant="h5">
-              <strong>Team: </strong>Front-end
-              <br />
-              <strong>Nhiệm vụ: </strong> Lập trình giao diện.
-            </Typography>
-          </CardContent>
         </Card>
-      </Grid>{' '} */}
-      {/* <Grid item xs={12} sm={6} md={4} lg={3}>
-        <Card
-          variant="outlined"
-          sx={{
-            maxWidth: 300,
-            height: 500,
-            borderWidth: 1,
-            borderColor: '#2196f3',
-          }}
-        >
-          <CardMedia
-            component="img"
-            image="img/dinhkhoa.jpg"
-            height="300"
-            title="Đình Khoa"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h4">
-              Nguyễn Đình Khoa - 20520586
-            </Typography>
-            <br />
-            <Typography variant="h5">
-              <strong>Team: </strong>Front-end
-              <br />
-              <strong>Nhiệm vụ: </strong> Lập trình giao diện.
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>{' '} */}
+      </Grid>
       <Grid item xs={12} sm={6} md={4} lg={3}>
         <Card
           variant="outlined"
           className="shadow"
           sx={{
             maxWidth: 300,
-
             borderWidth: 1,
           }}
         >
@@ -154,6 +89,7 @@ const CardMember = () => {
             image="img/long.jpg"
             height="300"
             title="Nhựt Long"
+            sx={{ height: 300 }}
           />
         </Card>
       </Grid>{' '}
@@ -163,7 +99,6 @@ const CardMember = () => {
           className="shadow"
           sx={{
             maxWidth: 300,
-
             borderWidth: 1,
           }}
         >
@@ -172,6 +107,7 @@ const CardMember = () => {
             image="img/loc.jpg"
             height="300"
             title="Tấn Lộc"
+            sx={{ height: 300 }}
           />
         </Card>
       </Grid>{' '}
