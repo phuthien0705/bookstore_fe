1bfb61d7f92c1e33a3032ffc1bd71e6354b70219
nakv <ng.anhkhoacontact@gmail.com>
2023-06-09 14:08:00 +0700

    fix orderby Orders

--- common/oderStatusMaping.tsx
+++ common/oderStatusMaping.tsx
@@ -14,31 +14,31 @@ const statusMaping = (
       return {
         content: 'Đang xử lý',
         color: '#FFB302',
-        icon: <PendingActionsIcon sx={{ color: 'black' }} />,
+        icon: <PendingActionsIcon sx={{ color: 'white' }} />,
       };
     case EOrderStatus.PAID:
       return {
         content: 'Đã thanh toán',
         color: '#FCE83A',
-        icon: <CreditScoreIcon sx={{ color: 'black' }} />,
+        icon: <CreditScoreIcon sx={{ color: 'white' }} />,
       };
     case EOrderStatus.SHIPPED:
       return {
         content: 'Đã lấy hàng, chuẩn bị giao',
         color: '#2DCCFF',
-        icon: <ContentPasteGoIcon sx={{ color: 'black' }} />,
+        icon: <ContentPasteGoIcon sx={{ color: 'white' }} />,
       };
     case EOrderStatus.DELIVERED:
       return {
         content: 'Giao thành công',
         color: '#56F000',
-        icon: <DoneIcon sx={{ color: 'black' }} />,
+        icon: <DoneIcon sx={{ color: 'white' }} />,
       };
     case EOrderStatus.CANCELED:
       return {
         content: 'Đã Hủy',
         color: '#A4ABB6',
-        icon: <ClearIcon sx={{ color: 'black'  }} />,
+        icon: <ClearIcon sx={{ color: 'white'  }} />,
       };
     default:
       return {
